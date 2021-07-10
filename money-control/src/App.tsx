abeeb9447b35ff8378e95631b5e65a2d8938e5d0
unknown <gabriel.cscanes@hotmail.com>
2021-07-10 00:02:47 -0300

    Criação do Header

--- money-control/src/App.tsx
+++ money-control/src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header />
       <h1>Teste</h1>
     </div>
   );
