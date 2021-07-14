f06e8568bd76ddb1052dfcaf23d402925c7e1edb
unknown <gabriel.cscanes@hotmail.com>
2021-07-14 02:52:44 -0300

    Criação do modal

--- money-control/src/App.tsx
+++ money-control/src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header/Header";
 import { Dashboard } from "./components/Dashboard/Dashboard";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen,
+     setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Modal isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+        <h2>Teste</h2>
+      </Modal>
+
       <Dashboard />
     </>
   );
